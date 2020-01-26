6bce9b06c8cfc508085573c0c2d015ff68922ed1
Kazuhiro Yamato <kz0817@gmail.com>
2020-01-27 00:21:02 +0900

    Brush up a help message.

--- qemu-cmd-gen.py
+++ qemu-cmd-gen.py
@@ -4,7 +4,65 @@ import subprocess
 import shlex
 import platform
 
-OPT_VNC_HELP = 'VNC number N. The port number: 5900 + N'
+DEFAULT_QEMU = 'qemu-system-x86_64'
+DEFAULT_NUM_CPU = 1
+DEFAULT_MEM_SIZE = 1024
+DEFAULT_GDB_PORT = 'tcp::1234'
+DEFAULT_SOUND_DEV = 'ac97'
+DEFAULT_MONITOR_DEV = 'stdio'
+
+OPT_QEMU_HELP = 'use QEMU as a command (Default: %s)' % DEFAULT_QEMU
+OPT_SMP_HELP = 'The number of CPUs (Default: %d)' % DEFAULT_NUM_CPU
+OPT_MEMORY_HELP = 'set memory size to M KiB (Default: %d)' % DEFAULT_MEM_SIZE
+OPT_NET_USER_HELP='create a NIC connected to user (private) network with NAT'
+OPT_HOST_FWD_HELP = '''
+forward a host port to guest. PORTS: host_port,guest_port (Ex: -f 8022,22).
+use with -u option.
+'''
+OPT_TAP_HELP = '''
+create a NIC connected to the host's standard bridge.
+To use this option, a root privilege is required
+'''
+OPT_BRIDGES_HELP = '''
+create NICs connected to host bridges. BR is a list of the bridges
+'''
+OPT_BRIDGE_HELPER_HELP = '''
+A bridge helper program which connects a VM's tap to a host bridge.
+(Default: /usr/lib/qemu/qemu-bridge-helper)
+Note: a root privilege is typicall required to use this option.
+'''
+OPT_SOUND_HELP = '''
+add a sound card. DEVICE is a sound card (default: %s).
+If this option is not given, no sound device is installed in the VM.
+''' % DEFAULT_SOUND_DEV
+OPT_INITRD_HELP = '''
+use the specified FILE as initrd/initramfs.
+This option is typically used with -k
+'''
+OPT_NOGRAPHIC_HELP = '''
+disable graphic and add a serial port redirected to the console
+'''
+OPT_BOOT_HELP = 'boot from DRIVE. DRIVE be c (Drive) or d (CD-ROM)'
+OPT_MENU_HELP = 'show a menu to select a boot device at BIOS screen'
+OPT_MONITOR_HELP = '''
+set the monitor device to DEV. If DEV is omitted, %s is used.
+''' % DEFAULT_MONITOR_DEV
+OPT_GDB_HELP = '''
+start GDB server with the specified PORT or default: %s
+''' % DEFAULT_GDB_PORT
+OPT_VNC_HELP = 'open VNC service. port: 5900 + N'
+OPT_USB_HELP = '''
+add a Host's USB device whose BUS and DEV can be found in output of "lsusb"
+'''
+OPT_EXEC_HELP = '''
+execute the generated command line.
+'''
+OPT_ADDITIONAL_HELP = '''
+additional QEMU options.
+Note: '--' is needed before the options when their first character is '-'.
+Ex: qemu-cmd-gen.py -d drive.img -- -vnc :0
+QEMU options can be seen at https://linux.die.net/man/1/qemu-kvm
+'''
 
 class Command(object):
     def __init__(self):
@@ -169,37 +227,53 @@ def generate(args):
 def start():
     parser = argparse.ArgumentParser(
         description='A tool for generating QEMU comand lines.')
-    parser.add_argument('--qemu', default='qemu-system-x86_64')
-    parser.add_argument('-s', '--smp', type=int)
-    parser.add_argument('-A', '--no-accel', action='store_true')
-    parser.add_argument('-m', '--memory', type=int, default=1024)
-    parser.add_argument('-d', '--disk-images', action='append', default=[])
-    parser.add_argument('-u', '--net-user', action='store_true')
+    parser.add_argument('--qemu', default=DEFAULT_QEMU, help=OPT_QEMU_HELP)
+    parser.add_argument('-s', '--smp', type=int, metavar='N', help=OPT_SMP_HELP)
+    parser.add_argument('-A', '--no-accel', action='store_true',
+                        help='disable accelaration such KVM')
+    parser.add_argument('-m', '--memory', type=int, default=DEFAULT_MEM_SIZE,
+                        metavar='M', help=OPT_MEMORY_HELP)
+    parser.add_argument('-d', '--disk-images', action='append', default=[],
+                        metavar='FILE', help='add FILE as a drive')
+    parser.add_argument('-u', '--net-user', action='store_true',
+                        help=OPT_NET_USER_HELP)
     parser.add_argument('-f', '--host-forward', action='append', default=[],
-      help='host_port,guest_port, Use with -u option.')
-    parser.add_argument('-t', '--tap', action='store_true')
-    parser.add_argument('-B', '--bridges', action='append',
-                        help='bridge names in which created taps are added')
+                        metavar='PORTS',
+                        help=OPT_HOST_FWD_HELP)
+    parser.add_argument('-t', '--tap', action='store_true', help=OPT_TAP_HELP)
+    parser.add_argument('-B', '--bridges', action='append', metavar='BR',
+                        help=OPT_BRIDGES_HELP)
     # This default path is for Ubuntu
     parser.add_argument('-H', '--bridge-helper',
-                        default='/usr/lib/qemu/qemu-bridge-helpera')
-    parser.add_argument('-c', '--cdrom')
-    parser.add_argument('-a', '--sound', type=str, nargs='?', default=None, const='ac97')
-    parser.add_argument('-k', '--kernel', nargs='+')
-    parser.add_argument('-i', '--initrd')
-    parser.add_argument('-n', '--nographic', action='store_true')
-    parser.add_argument('-b', '--boot', help='c: HDD, d: CDROM')
-    parser.add_argument('--menu', action='store_true',
-                        help='Show menu at boot up')
-    parser.add_argument('-M', '--monitor', nargs='?', const='stdio')
-    parser.add_argument('-g', '--gdb', nargs='?', const='tcp::1234')
-    parser.add_argument('-S', '--serial', choices=['mon:stdio', 'pts'])
-    parser.add_argument('-vnc', type=int, help=OPT_VNC_HELP)
-    parser.add_argument('-usb', nargs=2, type=int, help='Bus and Devices (can be found in output of lsusbj')
+                        default='/usr/lib/qemu/qemu-bridge-helper',
+                        metavar='HELPER', help=OPT_BRIDGE_HELPER_HELP)
+    parser.add_argument('-c', '--cdrom', metavar='FILE',
+                        help='add a CD-ROM drive with an ISO image file')
+    parser.add_argument('-a', '--sound', type=str, nargs='?', default=None,
+                        const=DEFAULT_SOUND_DEV, metavar='DEVICE',
+                        help=OPT_SOUND_HELP)
+    parser.add_argument('-k', '--kernel', nargs='+',
+                        metavar=('vmlinuz', 'cmdline'),
+                        help='boot Linux kernel directory')
+    parser.add_argument('-i', '--initrd', metavar='FILE', help=OPT_INITRD_HELP)
+    parser.add_argument('-n', '--nographic', action='store_true',
+                        help=OPT_NOGRAPHIC_HELP)
+    parser.add_argument('-b', '--boot', metavar='DRIVE', help=OPT_BOOT_HELP)
+    parser.add_argument('--menu', action='store_true', help=OPT_MENU_HELP)
+    parser.add_argument('-M', '--monitor', nargs='?', metavar='DEV',
+                        const=DEFAULT_MONITOR_DEV, help=OPT_MONITOR_HELP)
+    parser.add_argument('-g', '--gdb', metavar='PORT',
+                        nargs='?', const=DEFAULT_GDB_PORT, help=OPT_GDB_HELP)
+    parser.add_argument('-S', '--serial', choices=['mon:stdio', 'pts'],
+                        help='add a serial port')
+    parser.add_argument('-vnc', type=int, metavar='N', help=OPT_VNC_HELP)
+    parser.add_argument('-usb', nargs=2, type=int, metavar=('BUS', 'DEV'),
+                        help=OPT_USB_HELP)
 
-    parser.add_argument('-e', '--execute', action='store_true')
+    parser.add_argument('-e', '--execute', action='store_true',
+                        help=OPT_EXEC_HELP)
 
-    parser.add_argument('additional', nargs='*')
+    parser.add_argument('additional', nargs='*', help=OPT_ADDITIONAL_HELP)
 
     args = parser.parse_args()
     cmd = generate(args)
